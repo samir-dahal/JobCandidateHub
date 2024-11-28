fdd7ec946f87906fbd206de273fc08b7086ab626
Samir Dahal <samir.dahal@sea.sg>
2024-11-28 17:10:49 +0545

    Add project files.

---
+++ JobCandidateHub.API.Contracts/Requests/CandidateRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace JobCandidateHub.API.Contracts.Requests
+{
+    public class CandidateRequest
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+        public string BestTimeToCall { get; set; }
+        public string LinkedInProfileUrl { get; set; }
+        public string GitHubProfileUrl { get; set; }
+        public string Comment { get; set; }
+    }
+}
---
+++ JobCandidateHub.API/Controllers/CandidatesController.cs
@@ -0,0 +1,16 @@
+﻿using JobCandidateHub.API.Contracts.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobCandidateHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CandidatesController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> CandidateSignup(CandidateRequest request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
